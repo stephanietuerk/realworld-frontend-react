84808445dc0fc22cacde060b65b31d67750dd0df
Stephanie Tuerk <stephanie.tuerk@gmail.com>
2025-12-09 09:15:53 -0500

    add message about db clearing daily

--- src/features/feed/feed-skeleton/FeedSkeleton.tsx
+++ src/features/feed/feed-skeleton/FeedSkeleton.tsx
@@ -3,9 +3,10 @@ import styles from './FeedSkeleton.module.scss';
 export default function FeedSkeleton() {
   return (
     <div className={styles.loadingContainer}>
-  <div className={styles.skeletonCard} />
-  <div className={styles.skeletonCard} />
-  <div className={styles.skeletonCard} />
+      <div className={styles.skeletonCard} />
+      <div className={styles.skeletonCard} />
+      <div className={styles.skeletonCard} />
+      <div className={styles.skeletonCard} />
     </div>
   );
 }
